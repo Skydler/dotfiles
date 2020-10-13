ef5384deba5d55808e6919581a86b34e8e0ca842
Leonel Mandarino <leonelmandarino@gmail.com>
2020-10-13 19:46:30 -0300

    Update installation script

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env sh
 
-# Load submodules
-git submodule update --init -j 4
-
 # Save local stuff, just in case..
 mv ~/.bashrc ~/.bashrc.bak
 mv ~/.vim ~/.vim.bak
 mv ~/.vimrc ~/.vimrc.bak
+mv ~/.config/starship.toml ~/.config/starship.toml.bak
 
 # Create links on home folder
-ln -s ~/dotfiles/.bash-seafly-prompt/ ~/.bash-seafly-prompt
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.gitstatus ~/.gitstatus
 ln -s ~/dotfiles/.sensible.bash ~/.sensible.bash
 ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
+
+echo "Remember to install dependencies"
+echo "Dependencies list:"
+echo "-Starship"
+echo "-NeoVim"
