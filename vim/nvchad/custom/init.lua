ab0106d0929f9358aef0e0224ef80ea4bf99f526
Leonel Mandarino <leonel.mandarino@newfold.com>
2022-12-27 13:15:13 -0300

    Add some vim defaults configs

--- vim/nvchad/custom/init.lua
+++ vim/nvchad/custom/init.lua
@@ -1,8 +1,10 @@
-vim.opt.foldmethod="indent"
-vim.opt.foldlevelstart=99
-vim.opt.swapfile=false
 vim.g.mapleader = ","
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevelstart = 99
+vim.opt.swapfile = false
 vim.opt.tabstop = 2
+vim.opt.mousemodel = "extend"
+vim.opt.relativenumber = true
 
 vim.cmd("command W SudaWrite")
 nvchad.no_WhichKey_map()
