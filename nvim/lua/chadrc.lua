06c319c7eb32b7c492786d776e2fe9f0acaaa6ee
Leonel Mandarino <leonelmandarino@gmail.com>
2024-10-25 18:07:53 -0300

    Change menu entries

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -60,6 +60,25 @@ M = {
 
   nvdash = {
     load_on_startup = true,
+    buttons = {
+      { txt = "  New File", keys = "e", cmd = "ene" },
+      { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+      { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+      { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+      { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+      {
+        txt = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime) .. " ms"
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        end,
+        hl = "NvDashLazy",
+        no_gap = true,
+      },
+
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+    },
   },
 
   term = {
