82b8424aab936076688e5728d37c48ff7e4fa799
Leonel Mandarino <leonelmandarino@gmail.com>
2026-01-01 18:16:24 -0300

    Trying ty lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -22,36 +22,10 @@ local servers = {
   },
 
   ruff = {},
-  basedpyright = {
-    settings = {
-      basedpyright = {
-        -- Using Ruff's import organizer
-        disableOrganizeImports = true,
-        autoSearchPaths = true,
-        analysis = {
-          typeCheckingMode = "standard",
-        },
-      },
-    },
-  },
+  ty = {},
 }
 
 for name, opts in pairs(servers) do
   vim.lsp.config(name, opts)
   vim.lsp.enable(name)
 end
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client == nil then
-      return
-    end
-    if client.name == "ruff" then
-      -- Disable hover in favor of Pyright
-      client.server_capabilities.hoverProvider = false
-    end
-  end,
-  desc = "LSP: Disable hover capability from Ruff",
-})
